bb62b564766947e20fae64373da4036967454e70
Samreen Asif <studyproject870@gmail.com>
2024-05-07 09:49:47 +0500

    add file

---
+++ 14_/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let guestList = ['Sara', 'Faiza', 'Ali'];
+for (let i = 0; i < guestList.length; i++) {
+    console.log(`Dear ${guestList[i]}, \n You are warmly invited to dinner. `);
+}
---
+++ 14_/index.ts
@@ -0,0 +1,5 @@
+let  guestList : string[] = ['Sara','Faiza','Ali']
+
+for (let i = 0 ; i < guestList.length; i++){
+    console.log(`Dear ${guestList[i]}, \n You are warmly invited to dinner. `)
+}
