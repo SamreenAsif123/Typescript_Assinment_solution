c5510205967f94ce0c6aa24225ff2306456281b0
Samreen Asif <studyproject870@gmail.com>
2024-06-18 12:58:44 +0500

    Solution
    Solution of new assignment questions

---
+++ 20_List of Languages/index.js
@@ -0,0 +1,32 @@
+"use strict";
+let languages = [
+    "Urdu",
+    "English",
+    "Spanish",
+    "Mandarin",
+    "Hindi",
+    "Arabic",
+    "Punjabi",
+    "German",
+    "Korean",
+    "French",
+    "Telugu",
+    "Marathi",
+    "Turkish",
+    "Tamil",
+    "Vietnamese",
+    "Italian",
+    "Urdu",
+    "Persian",
+    "Gujarati",
+    "Polish",
+    "Ukrainian",
+    "Malayalam",
+    "Kannada",
+    "Romanian",
+    "Dutch",
+    "Greek"
+];
+languages.forEach((language) => {
+    console.log(language);
+});
---
+++ 20_List of Languages/index.ts
@@ -0,0 +1,33 @@
+let languages : string[]=  [
+    "Urdu",
+    "English",
+    "Spanish",
+    "Mandarin",
+    "Hindi",
+    "Arabic",
+    "Punjabi",
+    "German",
+    "Korean",
+    "French",
+    "Telugu",
+    "Marathi",
+    "Turkish",
+    "Tamil",
+    "Vietnamese",
+    "Italian",
+    "Urdu",
+    "Persian",
+    "Gujarati",
+    "Polish",
+    "Ukrainian",
+    "Malayalam",
+    "Kannada",
+    "Romanian",
+    "Dutch",
+    "Greek"
+  ]
+
+  languages.forEach((language)=>{
+    console.log(language)
+  })
+  
