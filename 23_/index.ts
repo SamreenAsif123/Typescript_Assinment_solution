b70e3d6e8e17a7b92893a737f0f57a51bb6f0ced
Samreen Asif <studyproject870@gmail.com>
2024-07-01 13:45:51 +0500

    update

---
+++ 23_/index.ts
@@ -0,0 +1,23 @@
+//Car information 
+let car = {
+    name : 'MySabaru',
+    make: 'Sabaru',
+    Model: 'Outback',
+    year: 2020,
+    color:'Black',
+    mileage: 15000,
+
+}
+
+//conditions 
+//Test that  evaluate to  true
+
+if (car.make =='Sabaru'? 'I predict true' ){
+    console.log('I predict true')
+}else {
+    console.log('I predict false')
+}
+
+
+
+
